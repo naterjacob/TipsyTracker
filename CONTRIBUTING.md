d745889e7729e8018b380b9a5142f469223beda8
Bernardo <bernalbernie@gmail.com>
2026-04-29 14:00:07 -0700

    Added IE as base, Prettier, and ESLint on frontend

---
+++ CONTRIBUTING.md
@@ -0,0 +1,3 @@
+jsut went with the deafults in pretty and what was in .prettierrc example, we can talk and come up with a better style guide
+
+Just copied my IE but deleted the express-backend from it as it was decided to go with another backend
---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+function MyApp() {
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter(index) {
+    deleteUser(characters[index]._id)
+      .then((res) => {
+        if(res.status === 204) {
+          const updated = characters.filter((character, i) => {
+            return i !== index;
+	});
+	  setCharacters(updated);
+      }
+      });
+  }
+
+
+  function updateList(person) {
+    postUser(person)
+      .then((res) => {
+	if(res.status === 201) return res.json();
+      })
+      .then((data) => { 
+	if(data) setCharacters([...characters, data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function deleteUser(_id) {
+    const promise = fetch(`http://localhost:8000/users/${_id}`, {
+      method: "DELETE"
+    });
+
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  },   []);
+
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form handleSubmit={updateList}/>
+    </div>
+  );
+}
+
+export default MyApp;
