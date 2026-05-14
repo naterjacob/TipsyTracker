7858874d1b73e7eab10aa5df7148ab1810846fa4
BernardoB03 <beranlbernie@gmail.com>
2026-05-14 08:08:02 -0700

    ran formatter and added comments on routing

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,24 @@
-import {Routes, Route, Outlet, Navigate} from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import LogIn from "./pages/logIn"
 import Home from "./pages/home"
 
 const ProtectedRoutes = () => {
+  //if user isnt logged in, return to login page, needs more logic but set to true to work with pages
   const user = true;
-  if (!user) return <Navigate to= "/"/>;
-  else return <Outlet/>;
+  if (!user) return <Navigate to="/" />;
+  else return <Outlet />;
 }
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LogIn/>} />
+        <Route path="/" element={<LogIn />} />
 
-        <Route element={<ProtectedRoutes/>}>
+        {/*Routes here just mean that, once a user is logged in it can access tha following pages*/}
+        <Route element={<ProtectedRoutes />}>
           <Route path="/home" element={<Home />} />
-          </Route>
+        </Route>
       </Routes>
     </>
   );
