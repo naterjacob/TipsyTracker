9ba2664a8021ff59e1bf0c4c04342128f83a75b9
Omar Garcia <omargarcia@Mac.lan>
2026-05-19 15:52:47 -0700

    Add home feed and post components

--- packages/frontend/src/pages/home.tsx
+++ packages/frontend/src/pages/home.tsx
@@ -1,10 +1,55 @@
 import Header from "../components/header"
-function Home() {
-    return (
-        <div>
-            <Header />
-        </div>
+import Feed from "../components/feed"
+import "./home.css"
 
+
+
+function Home() {
+
+    return (
+        <div className="home-page">
+            <Header />
+
+            <div className="home-content">
+
+                <Feed />
+
+                <div className="suggested-friends">
+
+                    <h3 className="suggested-friends-title">
+                        Suggested Friends:
+                    </h3>
+
+                    <div className="friend">
+                        <div className="friend-icon orange" />
+                        <p>Tucker E</p>
+                    </div>
+
+                    <div className="friend">
+                        <div className="friend-icon yellow" />
+                        <p>Adam R</p>
+                    </div>
+
+                    <div className="friend">
+                        <div className="friend-icon red" />
+                        <p>Jameson P</p>
+                    </div>
+
+                    <div className="friend">
+                        <div className="friend-icon blue" />
+                        <p>Reed S</p>
+                    </div>
+
+                    <div className="friend">
+                        <div className="friend-icon teal" />
+                        <p>Henry W</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
     );
+
 }
 export default Home;
